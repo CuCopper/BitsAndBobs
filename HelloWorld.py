9d9affe9438922c3f254766b4223ca4c7dba8b54
CuCopper <samp@pennym.plus.com>
2015-12-18 20:31:57 +0000

    A little program asking for name and age.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,10 @@
 print("Hello World")
+
+name = input("Hello Human, what is your name? ")
+
+print("Well hello there" + str(name) + " nice to meet you!")
+
+age = input("Please " + str(name) + " bestow upon me the knowledge of your age. ")
+
+print("How lovely. For I am " + str(age) + " aswell! By a happy conincidence!")
+print("Thanks for using me! I hope to see you again soon!")
